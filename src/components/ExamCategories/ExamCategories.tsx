ae8ca5d6c6a87ded5a14d8df869eed558644440d
saurabh080701 <rawatsaurabh885@gmail.com>
2024-02-20 15:02:32 +0530

    adding footer in hom page

--- src/components/ExamCategories/ExamCategories.tsx
+++ src/components/ExamCategories/ExamCategories.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import EXAM from '../../../backend/ExamCategories.json';
 import SingleExamCat from './SingleExamCat';
 
+const colors: string[] = ['#fceeda', '#fee5e5', '#d6f3fa', '#e9e2ff', '#defcd1'];
+
+const getRandomColor = () => {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+};
+
 const ExamCategories = () => {
 
   return (
@@ -13,7 +20,7 @@ const ExamCategories = () => {
 
       <main className='examsNamesBlocks'>
             {EXAM.map((exam , index) => (
-                <SingleExamCat key={index} exam={exam} />
+                <SingleExamCat key={index} exam={exam} divStyle={{ backgroundColor: getRandomColor() }}/>
               ))}
       </main>
     </section>
